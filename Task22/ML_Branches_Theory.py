5297e7ad32e43cb739b1d77ef02c8ed6a3b55297
dev-SaifUrRehman <125438631+coder-SaifUrRehman@users.noreply.github.com>
2023-05-02 21:57:20 +0500

    Add files via upload

---
+++ Task22/ML_Branches_Theory.py
@@ -0,0 +1,20 @@
+''' Supervised Learning: In supervised learning, the algorithm is trained on labeled data, which means the data 
+is already categorized and labeled. The algorithm learns from this labeled data to make predictions or
+decisions when it encounters new, unseen data. This type of learning is commonly used for tasks such as 
+classification or regression. '''
+
+''' Unsupervised Learning: In unsupervised learning, the algorithm is trained on unlabeled data, which means
+  the data is not categorized or labeled. The algorithm learns from this data to find patterns, 
+ relationships, or groupings within the data. This type of learning is commonly used for tasks such as 
+ clustering or anomaly detection.'''
+
+''' Semi-Supervised Learning: Semi-supervised learning is a combination of supervised and unsupervised learning.
+  In this type of learning, the algorithm is trained on a small amount of labeled data and a large amount of
+  unlabeled data. The algorithm learns from both types of data to make predictions or decisions when it 
+ encounters new, unseen data. This type of learning is commonly used when labeled data is difficult or
+  expensive to obtain.'''
+
+''' Reinforcement Learning: In reinforcement learning, the algorithm learns through trial and error. The
+  algorithm interacts with an environment and learns to take actions that maximize a reward signal. 
+ The algorithm learns from feedback in the form of rewards or penalties for its actions. This type of
+  learning is commonly used in robotics or gaming applications. '''
---
+++ Task24/Preprocessing and Feature_engg.py
@@ -0,0 +1,28 @@
+'''
+Data preprocessing involves cleaning and transforming raw data into a format that is suitable for machine 
+learning algorithms. This can include tasks such as removing missing values, handling outliers, scaling 
+features, and encoding categorical variables. The goal of data preprocessing is to improve the quality of the
+ data and make it more suitable for modeling.
+
+Data Preprocessing includes: 
+Handling missing data: filling in missing values with mean or median, or dropping missing values
+Handling outliers: removing or capping extreme values
+Scaling features: normalizing or standardizing data to have zero mean and unit variance
+Encoding categorical variables: converting categorical data to numerical format using techniques such as one-hot encoding or label encoding
+Handling imbalanced classes: balancing the number of samples in each class by oversampling or undersampling
+'''
+
+
+'''
+Feature engineering involves creating new features from the existing data that may be more informative or 
+useful for the model. This can include tasks such as extracting text features, creating interaction terms, 
+and transforming variables. Feature engineering can help improve the performance of the model by providing
+ more relevant and informative input.
+
+Feature Enggineering includes: 
+Creating new features: deriving new features from existing ones, such as calculating ratios or differences between variables
+Text feature extraction: converting text data into numerical format using techniques such as bag-of-words or TF-IDF
+Dimensionality reduction: reducing the number of features using techniques such as principal component analysis (PCA) or t-SNE
+Time series feature engineering: creating features that capture patterns and trends in time series data, such as rolling averages or lagged variables
+Interaction terms: creating new features by multiplying or adding existing features together to capture interactions between them.  
+'''
